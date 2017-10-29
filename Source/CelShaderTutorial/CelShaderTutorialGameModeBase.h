7567c589ec66c12bd7bff58544c6bac67b0a75d5
Joel Lopez <jowie.jowie@gmail.com>
2017-10-29 21:04:18 +0100

    Base project with imported Kano

---
+++ Source/CelShaderTutorial/CelShaderTutorialGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "CelShaderTutorialGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class CELSHADERTUTORIAL_API ACelShaderTutorialGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
